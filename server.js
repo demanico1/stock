067d38654392d953ae39cbdcf17c13ea2836a6ec
demanico1 <demanico@naver.com>
2025-04-14 08:42:21 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// public 폴더에서 정적 파일 제공 (index.html 포함)
+// 정적 파일 제공 (index.html 포함)
 app.use(express.static(path.join(__dirname, '.')));
 
-// 기본 경로에서 index.html 제공
+// 기본 라우트 처리
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
